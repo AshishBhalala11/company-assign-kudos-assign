e411fbab65188f405997f439866261fe24416aac
AshishBhalala11 <ashishbhalala2911@gmail.com>
2021-10-15 12:36:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect,useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  let time = new Date().toLocaleTimeString();
+  let seconds = new Date().getSeconds();
+  const[date,setDate] = useState(time);
+
+  const updateTime = () =>{
+    time = new Date().toLocaleTimeString();
+    setDate(time);
+    seconds = new Date().getSeconds();
+  }
+
+  setInterval(updateTime, 1000);
+
+  return(
+    <div className="clock">
+      <div className="time">
+        <span className={(seconds%2) == 0 ? 'time-span-black' : 'time-span-white' }>{date}</span>
+      </div>
+      <style>{ (seconds%2) == 0 ? 'body {background-color : black; color:white}' : 'body {background-color : white; color:black}' }</style>
     </div>
-  );
+
+  )
 }
 
 export default App;
+
+// class App extends Component{
+
+//   constructor(props){
+//     super(props)
+
+//     this.state = {
+//       date : new Date(),
+//     }
+//   }
+
+//     componentDidMount(){
+//       this.interval = setInterval(() => {
+//         this.setState({date : new Date()})
+//       }, 1000);
+//     }
+//     componentWillUnmount(){
+//       clearInterval(this.interval);
+//   }
+
+//   render(){
+//     return(
+//       <div className="clock">
+//         <h1 className="time">
+//           {this.state.date.toLocaleTimeString()}
+//         </h1>
+//       </div>
+//     )
+//   }
+// }
+
+// export default App;
